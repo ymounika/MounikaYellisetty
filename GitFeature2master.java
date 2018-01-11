0428c378a846859b505ba690074e94789756640c
ymounika <mounika.smile545@gmail.com>
2018-01-11 20:20:45 +0530

    renamedGitfeature2

--- GitFeature2master.java
+++ GitFeature2master.java
@@ -1,6 +1,6 @@
 package com.prokarma.GIT.MounikaYellisetty;
 
-public class GitFeature2 {
+public class GitFeature2master {
 public static void main(String[] args) {
 	System.out.println("Hi Feature2");
 	System.out.println("Hello Girl");
