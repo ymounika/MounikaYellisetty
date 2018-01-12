34c3b23314bce1caf4df5df8351b70546bdc6162
ymounika <mounika.smile545@gmail.com>
2018-01-12 14:12:28 +0530

    GitFeature2RenamedMaster

--- GitFeature2MasterRename.java
+++ GitFeature2MasterRename.java
@@ -1,6 +1,6 @@
 package com.prokarma.GIT.MounikaYellisetty;
 
-public class GitFeature2 {
+public class GitFeature2MasterRename {
 public static void main(String[] args) {
 	System.out.println("Hi Feature2");
 	System.out.println("Hi Modify Me!!!");
