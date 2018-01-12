00296017d73773088bfa1274858c74f0aff43d1b
ymounika <mounika.smile545@gmail.com>
2018-01-12 12:33:58 +0530

    feture3

--- GitFeature2master.java
+++ GitFeature2master.java
@@ -2,10 +2,6 @@ package com.prokarma.GIT.MounikaYellisetty;
 
 public class GitFeature2Master {
 public static void main(String[] args) {
-	System.out.println("Hi Feature2");
-	System.out.println("Hi Modify Me!!!");
-	System.out.println("Hello Girl");
-	System.out.println("HelloHi Boy");
-	System.out.println("HelloHiHiByeBye Both");
+	System.out.println("this is feture 3");
 }
 }
